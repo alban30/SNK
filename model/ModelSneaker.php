c45ecc32e085c6cd8e540820ca1aebeb6ce7e28b
Jeremie Demas <jeremie.demas@gmail.com>
2018-11-30 14:33:16 +0100

    UPDATE MODELSNEAKER

--- model/ModelSneaker.php
+++ model/ModelSneaker.php
@@ -1,46 +1,74 @@
 <?php
 
-require_once (File::build_path(array("model", "Model.php")));
+require_once (File::build_path(array("model","Model.php")));
 
 class ModelSneaker extends Model {
     protected static $object = "sneaker";
-    protected static $primary='';
+    protected static $primary="id_sneaker";
 
-    private $marque;
-    private $couleur;
-    private $immatriculation;
+    private $id_sneaker;
+    private $nom_sneaker;
+    private $prix_sneaker;
+    private $couleur_sneaker;
+    private $pointure_sneaker;
+    private $id_marque;
 
-    public function getMarque() {
-        return $this->marque;
+    public function getIdSneaker() {
+        return $this->id_sneaker;
     }
 
-    public function setMarque($marque2) {
-        $this->marque = $marque2;
+    public function setIdSneaker($idSneaker) {
+        $this->id_sneaker = $idSneaker;
     }
 
-    public function getCouleur() {
-        return $this->couleur;
+    public function getNomSneaker() {
+        return $this->nom_sneaker;
     }
 
-    public function setCouleur($couleur2) {
-        $this->couleur  = $couleur2;
+    public function setNomSneaker($nomSneaker) {
+        $this->nom_sneaker = $nomSneaker;
     }
 
-    public function getImmatriculation() {
-        return $this->immatriculation;
+    public function getPrixSneaker() {
+        return $this->prix_sneaker;
     }
 
-    public function setImmatriculation($immatriculation2) {
-        if(strlen($immatriculation2) <= 8) {
-            $this->immatriculation = $immatriculation2;
-        }
+    public function setPrixSneaker($prixSneaker) {
+        $this->prix_sneaker = $prixSneaker;
+    }
+
+    public function getCouleurSneaker() {
+        return $this->couleur_sneaker;
+    }
+
+    public function setCouleurSneaker($couleurSneaker) {
+        $this->couleur_sneaker = $couleurSneaker;
+    }
+
+    public function getPointureSneaker() {
+        return $this->pointure_sneaker;
+    }
+
+    public function setPointureSneaker($pointureSneaker) {
+        $this->pointure_sneaker = $pointureSneaker;
+    }
+
+    public function getIdMarque() {
+        return $this->id_sneaker;
+    }
+
+    public function setIdMarque($idMarque) {
+        $this->id_marque = $idMarque;
     }
 
     public function __construct($data = array()) {
         if (!empty($data)) {
-            $this->marque = $data("marque");
-            $this->couleur = $data("couleur");
-            $this->immatriculation = $data("immatriculation");
+            $this->id_sneaker = $data("idSneaker");
+            $this->nom_sneaker = $data("nomSneaker");
+            $this->prix_sneaker = $data("prixSneaker");
+            $this->couleur_sneaker = $data("couleurSneaker");
+            $this->pointure_sneaker = $data("pointureSneaker");
+            $this->id_marque = $data("idMarque");
         }
     }
 
