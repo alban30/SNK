55ec98e108443ac1765e54ac8b134aa03c530660
Jeremie Demas <jeremie.demas@gmail.com>
2018-12-06 17:55:27 +0100

    UPDATE ACCESSOIRE

--- view/accessoire/update.php
+++ view/accessoire/update.php
@@ -2,23 +2,23 @@
         <fieldset>
                 <legend>Mon formulaire :</legend>
                 <p>
-                        <label for="id_accessoire">ID Accessoire</label> :
-                        <input type="number" placeholder="Ex : 1" name="id_accessoire" id="id_accessoire" value="<?php echo htmlspecialchars($a->get("id_accessoire"));?>" <?php echo $modifier; ?>/>
+                        <label for="idAccessoire">ID Accessoire</label> :
+                        <input type="number" placeholder="Ex : 1" name="idAccessoire" id="idAccessoire" value="<?php echo htmlspecialchars($a->get("id_accessoire"));?>" <?php echo $modifier; ?>/>
                 </p>
 
                 <p>
-                        <label for="nom_accessoire">Nom Accessoire</label> :
-                        <input type="text" placeholder="Ex : Stan Smith" name="nom_accessoire" id="nom_accessoire" value="<?php echo htmlspecialchars($a->get("nom_accessoire"));?>" required/>
+                        <label for="nomAccessoire">Nom Accessoire</label> :
+                        <input type="text" placeholder="Ex : Stan Smith" name="nomAccessoire" id="nomAccessoire" value="<?php echo htmlspecialchars($a->get("nom_accessoire"));?>" required/>
                 </p>
 
                 <p>
-                        <label for="prix_accessoire">Prix Accessoire</label> :
-                        <input type="number" placeholder="Ex : 100" name="prix_accessoire" id="prix_accessoire" value="<?php echo htmlspecialchars($a->get("prix_accessoire"));?>" required/>
+                        <label for="prixAccessoire">Prix Accessoire</label> :
+                        <input type="number" placeholder="Ex : 100" name="prixAccessoire" id="prixAccessoire" value="<?php echo htmlspecialchars($a->get("prix_accessoire"));?>" required/>
                 </p>
 
                 <p>
-                        <label for="marque_accessoire">Marque Accessoire</label> :
-                        <input type="text" placeholder="Ex : Nike" name="marque_accessoire" id="marque_accessoire" value="<?php echo htmlspecialchars($a->get("marque_accessoire"));?>" required/>
+                        <label for="marqueAccessoire">Marque Accessoire</label> :
+                        <input type="text" placeholder="Ex : Nike" name="marqueAccessoire" id="marqueAccessoire" value="<?php echo htmlspecialchars($a->get("marque_accessoire"));?>" required/>
                 </p>
 
                 <p>
