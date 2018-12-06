a612a7acf39df45ffe98ee70aef84acd0a1955a9
Alban Pereira <alban.pereira98@gmail.com>
2018-12-06 14:58:15 +0100

    ADD MULTIPLE

--- config/Conf.php
+++ config/Conf.php
@@ -2,30 +2,30 @@
 class Conf {
 
     static private $databases = array(
-        // Le nom d'hote est webinfo a l'IUT
-        // ou localhost sur votre machine
-        'hostname' => 'webinfo.iutmontp.univ-montp2.fr',
-        // A l'IUT, vous avez une BDD nommee comme votre login
-        // Sur votre machine, vous devrez creer une BDD
-        'database' => 'pereiraa',
-        // A l'IUT, c'est votre login
-        // Sur votre machine, vous avez surement un compte 'root'
-        'login' => 'pereiraa',
-        // A l'IUT, c'est votre mdp (INE par defaut)
-        // Sur votre machine personelle, vous avez creez ce mdp a l'installation
-        'password' => 'rendpasfou34'
+            // Le nom d'hote est webinfo a l'IUT
+            // ou localhost sur votre machine
+            'hostname' => 'webinfo.iutmontp.univ-montp2.fr',
+            // A l'IUT, vous avez une BDD nommee comme votre login
+            // Sur votre machine, vous devrez creer une BDD
+            'database' => 'pereiraa',
+            // A l'IUT, c'est votre login
+            // Sur votre machine, vous avez surement un compte 'root'
+            'login' => 'pereiraa',
+            // A l'IUT, c'est votre mdp (INE par defaut)
+            // Sur votre machine personelle, vous avez creez ce mdp a l'installation
+            'password' => 'rendpasfou34'
     );
 
     static private $debug = False;
 
     static public function getLogin() {
-        //en PHP l'indice d'un tableau n'est pas forcement un chiffre.
-        return self::$databases['login'];
+            //en PHP l'indice d'un tableau n'est pas forcement un chiffre.
+            return self::$databases['login'];
     }
 
     static public function getHostname() {
-        //en PHP l'indice d'un tableau n'est pas forcement un chiffre.
-        return self::$databases['hostname'];
+            //en PHP l'indice d'un tableau n'est pas forcement un chiffre.
+            return self::$databases['hostname'];
     }
 
     static public function getDatabase() {
--- view/view.php
+++ view/view.php
@@ -1,31 +1,55 @@
 <!DOCTYPE html>
 <html>
-        <head>
-                <meta charset="UTF-8">
-                <title><?php echo $pagetitle; ?></title>
-        </head>
-        <body>
-                <header>
-                        <nav style="border: 1px solid black;text-align:center;padding:1%;">
-                                <a href="index.php?action=readAll&controller=voiture">Les Voitures</a>
-                                <a href="index.php?action=readAll&controller=utilisateur">Les Utilisateurs</a>
-                                <a href="index.php?action=readAll&controller=trajet">Les Trajets</a>
-                                <a href="config/preference.html">Préférence</a>
-                                <?php Session::getUserMenu(); ?>
-                        </nav>
-                </header>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="css/styles.css" media="all"/>
+        <title><?php echo $pagetitle; ?></title>
+    </head>
+    <body>
+        <header>
+            <nav>
+                <div>
+                    <div>
+                        <a><img alt="Salut" src="img/logo.jpg"/></a>
+                    </div>
+                </div>
+                <ul class="btw">
+                    <li>
+                        <a class="ici" href="index.php">Accueil</a>
+                    </li>
+                    <li>
+                        <a href="index.php?action=readAll&controller=sneaker">Sneakers</a>
+                    </li>
+                    <li>
+                        <a href="index.php?action=readAll&controller=other">Dérivés</a>
+                    </li>
+                    <li>
+                        <a href="config/preference.html">Préférence</a>
+                    </li>
+            </nav>
+        </header>
 
-                <?php
-                // Si $controleur='voiture' et $view='list',
-                // alors $filepath="/chemin_du_site/view/voiture/list.php"
-                $filepath = File::build_path(array("view", static::$object, "$view.php"));
-                require $filepath;
-                ?>
+        <main>
+            <?php
+            // Si $controleur='voiture' et $view='list',
+            // alors $filepath="/chemin_du_site/view/voiture/list.php"
+            $filepath = File::build_path(array("view", static::$object, "$view.php"));
+            require $filepath;
+            ?>
+        </main>
 
-                <footer>
-                        <p style="border: 1px solid black;text-align:right;padding-right:1em;">
-                                Site de covoiturage de ...
-                        </p>
-                </footer>
-        </body>
+        <footer>
+            <a href="https://www.facebook.com/" target="_blank">
+                <img class="social" src="img/icon/facebook.png" alt="fb">
+            </a>
+            <a href="https://twitter.com/" target="_blank">
+                <img class="social" src="img/icon/twitter.png" alt="twitter">
+            </a>
+            <a href="https://www.google.fr/" target="_blank">
+                <img class="social" src="img/icon/google.png" alt="google">
+            </a>
+            <p>Made with <img src="img/heart.png" alt="heart"> in Montpellier</p>
+            <p>&copy; SNK 2018-2019 | Tous droits réservés.</p>
+        </footer>
+    </body>
 </html>
