eac3e73cc8038562ac281c52a3139d0cabbc38a3
Pereira Alban <43809537+alban30@users.noreply.github.com>
2018-12-08 18:40:08 +0100

    UPDATE CSS & VIEW

--- view/view.php
+++ view/view.php
@@ -12,17 +12,17 @@
                     <ul class="social left">
                         <li>
                           <a href="https://www.facebook.com/" target="_blank">
-                              <img class="social" src="img/icon/facebook.png" alt="fb">
+                              <img src="img/icon/facebook.png" alt="fb">
                           </a>
                         </li>
                         <li>
                           <a href="https://twitter.com/" target="_blank">
-                              <img class="social" src="img/icon/twitter.png" alt="twitter">
+                              <img src="img/icon/twitter.png" alt="twitter">
                           </a>
                         </li>
                         <li>
                           <a href="https://www.google.fr/" target="_blank">
-                              <img class="social" src="img/icon/google.png" alt="google">
+                              <img src="img/icon/google.png" alt="google">
                           </a>
                         </li>
                     </ul>
@@ -31,7 +31,7 @@
                             <a>0647857007</a>
                         </li>
                     </ul>
-                    <ul class="menu left">
+                    <ul class="menu right">
                         <?php Session::getUserMenu(); ?>
                         <li>
                             <a href="index.php?controller=accessoire&action=readAll">Panier</a>
@@ -66,13 +66,13 @@
 
         <footer>
             <a href="https://www.facebook.com/" target="_blank">
-                <img class="social" src="img/icon/facebook.png" alt="fb">
+                <img src="img/icon/facebook.png" alt="fb">
             </a>
             <a href="https://twitter.com/" target="_blank">
-                <img class="social" src="img/icon/twitter.png" alt="twitter">
+                <img src="img/icon/twitter.png" alt="twitter">
             </a>
             <a href="https://www.google.fr/" target="_blank">
-                <img class="social" src="img/icon/google.png" alt="google">
+                <img src="img/icon/google.png" alt="google">
             </a>
             <p>Made with <img src="img/heart.png" alt="heart"> in Montpellier</p>
             <p>&copy; SNK - World Sneakers 2018-2019 | Tous droits réservés.</p>
