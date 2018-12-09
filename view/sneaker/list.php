44c6dddefcd6299b8d2aeaecad9d7f8a8c87ef34
Pereira Alban <43809537+alban30@users.noreply.github.com>
2018-12-09 23:22:27 +0100

    UPDATE VIEW SNEAKERS

--- view/sneaker/list.php
+++ view/sneaker/list.php
@@ -1,8 +1,20 @@
 <?php
-if(Session::is_admin()) {
-    echo '<a style="margin-right: 1%" href="index.php?controller=sneaker&action=create">Créer une sneaker</a>';
-}
-foreach ($tab_s as $s) {
-    echo '<p><a href="index.php?controller=sneaker&action=read&idSneaker=' . rawurlencode($s->get("id_sneaker")) . '"> Sneaker d\'id ' . htmlspecialchars($s->get("id_sneaker")) . '.</a></p>';
-}
+if(Session::is_admin()): ?>
+    <article class="content ">
+        <a href="index.php?controller=sneaker&action=create">Créer une sneaker</a>
+    </article>
+<?php endif ?>
+<?php
+foreach ($tab_s as $s): ?>
+    <article class="content product">
+        <div class="contenu">
+            <a href="index.php?controller=sneaker&action=read&idSneaker=<?php echo rawurlencode($s->get("id_sneaker")) ?>">
+              <img src="img/product/<?php echo $s->get("id_sneaker") ?>.png" alt="product <?php echo $s->get("id_sneaker") ?>">
+              <h3><strong><?php echo $s->get("nom_sneaker") ?></strong></h3>
+              <p><?php echo $s->get("marque_sneaker") ?></p>
+              <p><i></i><?php echo $s->get("prix_sneaker") ?> €</p>
+            </a>
+        </div>
+    </article>
+<?php endforeach ?>
 ?>
