3af49fabe3ac11e802819bf8ac3165283595954d
Pereira Alban <43809537+alban30@users.noreply.github.com>
2018-12-09 21:09:49 +0100

    UPDATE DESIGN

--- view/accueil/accueil.php
+++ view/accueil/accueil.php
@@ -3,6 +3,12 @@
 				<img src="img/zoomfly.jpg" alt="exo"/>
         <div class="titre"><h2>ZOOM FLY SP</h2></div>
         <div class="texte">La dernière sortie de Nike x Off White</div>
-        <div class="button"><input type="submit" value="DECOUVRIR" /></div>
+        <div class="button button1"><input type="submit" value="DECOUVRIR" /></div>
 		</div>
+    <div class="first">
+        <img src="img/yeezy.jpg" alt="exo"/>
+        <div class="titre2"><h2>ZOOM FLY SP</h2></div>
+        <div class="texte2">La dernière sortie de Nike x Off White</div>
+        <div class="button button2"><input type="submit" value="DECOUVRIR" /></div>
+    </div>
 </article>
