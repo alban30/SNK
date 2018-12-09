6177413eba0543fd116e7e0758a2f62d93eadbc0
MehdiSaadi <43638052+MehdiSaadi@users.noreply.github.com>
2018-12-09 13:55:55 +0100

    UPDATE PANIER INTO SNEAKER CONTROLLER

--- lib/Session.php
+++ lib/Session.php
@@ -16,5 +16,39 @@ class Session {
             echo '<li><a href="index.php?controller=utilisateur&action=connect">Connexion</a></li>';
         }
     }
+
+    public function creationPanier() {
+            if (!isset($_SESSION["panier"])) {
+                    $_SESSION["panier"] = array();
+                    $_SESSION["panier"]["objet"] = array();
+                    $_SESSION["panier"]["quantite"] = array();
+                    $_SESSION["panier"]["cle"] = false;
+            }
+            return true;
+
+    }
+
+    public function isVerrouille(){
+        if (isset($_SESSION["panier"]) && $_SESSION["panier"]["cle"]) {
+                return true;
+        }
+        else {
+                return false;
+        }
+    }
+
+    public function viderPanier(){
+
+        unset($_SESSION['panier']);
+    }
+
+    public  function montantPanier(){
+            $prixtotal=0;
+            for($i = 0; $i < count($_SESSION["panier"]["objet"]); $i++) {
+                    $prixtotal += $_SESSION["panier"]["quantite"][$i] * $_SESSION["panier"]["objet"][$i];
+            }
+            return $prixtotal;
+    }
+
 }
 ?>
