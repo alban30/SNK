4f9906296f399a58a98ebac9d1240d524abc1c19
Alban Pereira <alban.pereira98@gmail.com>
2018-12-10 13:31:06 +0100

    ADD IMG & UPDATE CSS

--- view/sneaker/list2.php
+++ view/sneaker/list2.php
@@ -1,16 +1,17 @@
 <?php
 if(Session::is_admin()): ?>
-    <div class="contenu">
+    <div class="">
         <a href="index.php?controller=sneaker&action=create">Créer une sneaker</a>
     </div>
 <?php endif ?>
-<article class="content product">
-    <?php foreach ($tab_s as $s): ?>
+<?php
+foreach ($tab_s as $s): ?>
+    <article class="content product">
         <?php
         $i = 0;
         if($i%3 == 0):
         ?>
-        <div>
+        <div class="contenu">
             <a href="index.php?controller=sneaker&action=read&idSneaker=<?php echo rawurlencode($s->get("id_sneaker")) ?>">
               <img src="img/product/<?php echo $s->get("id_sneaker") ?>.png" alt="product <?php echo $s->get("id_sneaker") ?>">
               <h3><strong><?php echo $s->get("nom_sneaker") ?></strong></h3>
@@ -22,5 +23,5 @@ if(Session::is_admin()): ?>
         endif;
         $i++;
         ?>
-    <?php endforeach ?>
-</article>
+    </article>
+<?php endforeach ?>
