7cdebbd591cbe3f42f25195ef103736b31a44835
Pereira Alban <43809537+alban30@users.noreply.github.com>
2018-12-13 19:27:41 +0100

    UPDATE MULTIPLE

--- view/sneaker/detail.php
+++ view/sneaker/detail.php
@@ -5,20 +5,25 @@ $prix_sneaker_html = htmlspecialchars($s->get("prix_sneaker"));
 $couleur_sneaker_html = htmlspecialchars($s->get("couleur_sneaker"));
 $pointure_sneaker_html = htmlspecialchars($s->get("pointure_sneaker"));
 $marque_sneaker_html = htmlspecialchars($s->get("marque_sneaker"));
-
-if(Session::is_admin()) {
-	echo '<a style="margin-right: 1%" href="index.php?controller=sneaker&action=delete&idSneaker=' . rawurlencode($s->get("id_sneaker")) . '">Supprimer cette sneaker</a><a style="margin-right: 1%" href="index.php?controller=sneaker&action=update&idSneaker=' . rawurlencode($s->get("id_sneaker")) . '">Modifier cette sneaker</a>';
-}
 ?>
 
-<article class="content detail">
 <!-- 	<?php
 	echo "<p> Id : $id_sneaker_html, nom : $nom_sneaker_html, prix : $prix_sneaker_html, couleur : $couleur_sneaker_html, pointure : $pointure_sneaker_html, marque : $marque_sneaker_html</p>";
 
 	echo '<a  href="index.php?controller=panier&action=addPanier&idSneaker=' . $id_sneaker_html . '">Ajouter cette sneaker au panier</a>';
 	?> -->
-	<div><img src="img/product/sneakers/<?php echo $id_sneaker_html ?>.png" alt="product <?php echo $id_sneaker_html ?>"></div>
-	<div>cc</div>
-	<div>cc</div>
 
+
+<article class="content detail">
+	<img src="img/product/sneakers/<?php echo $id_sneaker_html ?>.png" alt="product <?php echo $id_sneaker_html ?>"></div>
+</article>
+
+<article class="content">
+	<div><?php if(Session::is_admin()) {echo '<a href="index.php?controller=sneaker&action=delete&idSneaker=' . rawurlencode($s->get("id_sneaker")) . '">Supprimer cette sneaker</a><a href="index.php?controller=sneaker&action=update&idSneaker=' . rawurlencode($s->get("id_sneaker")) . '">Modifier cette sneaker</a>';} ?></div>
+	<div>
+		<div><?php echo $marque_sneaker_html ?></div>
+		<div><?php echo $nom_sneaker_html ?></div>
+		<div><?php echo $prix_sneaker_html ?> €</div>
+		<div class="button button2"><input type="submit" value="Ajouter au panier" /></div>
+	</div>
 </article>
