7588e9f984db356d94abf9aac5b02e796efd6359
Alban Pereira <alban.pereira98@gmail.com>
2018-12-14 13:56:24 +0100

    UPDATE MULTIPLE

--- view/accueil/accueil.php
+++ view/accueil/accueil.php
@@ -1,14 +1,14 @@
 <article class="content">
 		<div class="first">
-				<img src="img/zoomfly.jpg" alt="exo"/>
-        <div class="titre"><h2>ZOOM FLY SP</h2></div>
-        <div class="texte">La dernière sortie de Nike x Off White</div>
-        <div class="button button1"><input type="submit" value="DECOUVRIR" /></div>
+			<img src="img/zoomfly.jpg" alt="exo"/>
+            <div class="titre"><h2>ZOOM FLY SP</h2></div>
+            <div class="texte">La dernière sortie de Nike x Off White</div>
+            <div class="button button1"><a href="index.php?controller=sneaker&action=read&idSneaker=1"><input type="button" value="DECOUVRIR" /></a></div>
 		</div>
     <div class="first">
         <img src="img/yeezy.jpg" alt="exo"/>
         <div class="titre2"><h2>YEEZY 350 V2 ZEBRA</h2></div>
         <div class="texte2">Le dernier restock de adidas x yeezy</div>
-        <div class="button button2"><input type="submit" value="DECOUVRIR" /></div>
+        <div class="button button2"><a href="index.php?controller=sneaker&action=read&idSneaker=2"><input type="button" value="DECOUVRIR" /></a></div>
     </div>
 </article>
